c18308e5a657ace99a9a35d6f35bd54fefdff9a5
u6k <u6k.apps@gmail.com>
2012-10-08 21:38:45 +0900

    パッケージを整理、ライセンス情報を付加。

---
+++ src/sample-gcm-server/src/main/java/jp/gr/java_conf/u6k/sample_gcm_server/Main.java
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2012 Yuichi Naono (u6k.yu1.main@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package jp.gr.java_conf.u6k.sample_gcm_server;
+
+import java.io.IOException;
+
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Result;
+import com.google.android.gcm.server.Sender;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String key = "AIzaSyB_6rJ-Vwb0TcixmdPEWb3sRgmc_2EBvCo";
+        String registrationId = "APA91bHXeRcx8YYYcZukYwEEpqlaGKDErAYQGZ8fcKBmVMEExSz-Ix23QVDwlEOaNfaAGXTS37D-1p2YTSC3pf7tEzMUSy2DCx9qpHfDkJQG6RwheTq54t-CdX8AFB4H4uP34CSChRHxAQAnh5iRwDBHAzs4_tFc9Q";
+
+        Sender sender = new Sender(key);
+        Message message = new Message.Builder().addData("message", "てすと").build();
+        Result result = sender.send(message, registrationId, 1);
+        System.out.println(result.toString());
+    }
+
+}
